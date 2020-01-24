b97e43752f639107fdb0d02b9de5e34788ac7757
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-01-24 00:42:26 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
+require 'pp'
 # Call the method directors_database to retrieve the NDS
 directors_database
 def pretty_print_nds(nds)
